d7502c3bde4f3f17eaffc7e6377b2d0657641889
Łukasz Kamiński <lukasz.kaminski@bisnode.com>
2019-09-13 22:26:37 +0200

    Move project to root directory

---
+++ src/main/java/com/bordgejmer/user/User.java
@@ -0,0 +1,10 @@
+package com.bordgejmer.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class User {
+    private final String name;
+}
---
+++ src/main/java/com/bordgejmer/user/UserCreatingController.java
@@ -0,0 +1,8 @@
+package com.bordgejmer.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/user")
+public class UserCreatingController {
+
+}
